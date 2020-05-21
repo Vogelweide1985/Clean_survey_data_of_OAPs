3bfc0ad387adce8420f39ca078cd80bbef7cdb92
vogelweide1985 <vogelweide1985@hotmail.de>
2020-05-21 19:56:38 +0200

    'Exploring total Time

---
+++ explo.r
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(lubridate)
+
+#Load Data
+df <- readRDS("./public_data/survey.rds")
+
+#Renaming cols
+colnames(df) <- gsub("Zeitstempel", "timestamp", colnames(df))
+
+#Transmute timestamps to lubridate duration
+df <- df %>%
+      select(starts_with("timestamp")) %>%
+      transmute_all(duration)
+
+
+#Create unique ID
+df$id <- 1:length(1:nrow(df))
+
+#Summarise of total time -> "timestamp_5"
+summary(df$timestamp_5)
+
+#Plot ID vs. total_time
+ggplot(df, aes(x=id, y= dminutes(timestamp_5))) + geom_point()
+
+#Plot Boxplot
+ggplot(df, aes(y=dminutes(timestamp_5))) + geom_boxplot()
+
+#Plot Histogram
+ggplot(df, aes(x=dminutes(timestamp_5))) + geom_histogram(binwidth=50)
