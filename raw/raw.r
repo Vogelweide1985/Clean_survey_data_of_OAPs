e27f57e560631ad9865a6d9427dbb582a032ed6d
vogelweide1985 <vogelweide1985@hotmail.de>
2020-05-22 16:47:37 +0200

    added geschlecht, alter, selection_var

--- raw/raw.r
+++ raw/raw.r
@@ -4,7 +4,11 @@ library(haven)
 
 
 df <- read_spss("./raw/Daten_ZMG_SmartHome_ifak.sav")
-df <- df %>% select(contains("zeit"))
+str(df)
+
+
+df <- df %>% select(contains("zeit"), gender = F1, age_group = F2r, media_filter = F6_671)
 
 
 saveRDS(df, "./public_data/survey.rds")
+
