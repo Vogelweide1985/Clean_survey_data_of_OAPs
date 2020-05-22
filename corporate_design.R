56679a5711e8a755d54a4bd15163e550ac03146a
vogelweide1985 <vogelweide1985@hotmail.de>
2020-05-22 22:21:26 +0200

    Early finish of CD

--- corporate_design.R
+++ corporate_design.R
@@ -7,20 +7,42 @@ cd_theme <- function(base_size = 11,
       theme_minimal(base_size = base_size, 
                     base_family = base_family,
                     base_line_size = base_line_size) %+replace%
-            theme(
+            theme(plot.background = element_rect(
+                  fill = "#F1FFE7"
+                  ),
                   plot.title = element_text(
                         color = rgb(25, 43, 65, maxColorValue = 255), 
                         face = "bold",
-                        hjust = 0),
+                        hjust = 0.001,
+                        vjust = 2),
+                  plot.subtitle = element_text(
+                        color = rgb(25, 43, 65, maxColorValue = 255), 
+                        face = "plain",
+                        hjust = 0.005,
+                        size = 11,
+                        vjust = 1),
                   axis.title = element_text(
-                        color = rgb(105, 105, 105, maxColorValue = 255),
-                        size = rel(0.75)),
+                        size = rel(1),
+                        face = "bold"),
                   axis.text = element_text(
                         color = rgb(105, 105, 105, maxColorValue = 255),
-                        size = rel(0.5)),
-                  panel.grid.major = element_line(
+                        size = rel(1)),
+                  axis.line  = element_line(
+                        linetype = "solid", 
+                        colour = rgb(105, 105, 105, maxColorValue = 255),
+                        size = rel(1)
+                  ),
+                  axis.line.y  = element_line(
+                        linetype = "blank", 
+                        colour = rgb(105, 105, 105, maxColorValue = 255),
+                        size = rel(1)
+                  ),
+                  panel.grid.major.x = element_line(
                         rgb(105, 105, 105, maxColorValue = 255),
                         linetype = "blank"),   
+                  panel.grid.major.y = element_line(
+                        rgb(105, 105, 105, maxColorValue = 255),
+                        linetype = "dotted", size =0.5),   
                   panel.grid.minor = element_line(
                         rgb(105, 105, 105, maxColorValue = 255),
                         linetype = "blank", 
@@ -30,8 +52,17 @@ cd_theme <- function(base_size = 11,
             )
 }
 
-
-cd_main <- c( "#337CA0", "#C1FF31", "#DAC4F7", "#CF5C36", "#F1FFE7")
+cd_background <- c("#F1FFE7")
+cd_main <- c( "#337CA0", "#C1FF31", "#DAC4F7", "#CF5C36")
 cd_gradient <- c("337CA0", "438B94", "539987", "62A87B", "72B66F", "82C562", 
                  "92D356", "92D356", "A1E24A", "B1F03D", "C1FF31")
 cd_diverging <- "tbd"
+
+
+### Tests ####
+
+ggplot(iris, aes(x= Species, y = Sepal.Length)) + geom_boxplot(fill = cd_main[1:3]) + cd_theme() +
+      ggtitle("This is a Title", "This is a subtitle") 
+
+ggplot(iris, aes(x= 1:nrow(iris), y = Sepal.Length)) + geom_point(color = cd_main[1]) + cd_theme() +
+      ggtitle("This is a Title", "This is a subtitle") 
